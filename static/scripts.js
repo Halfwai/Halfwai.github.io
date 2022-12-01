71abed43f262e90f76ccd21a015379351e2a9efc
Halfwai <wai_sanlee@hotmail.com>
2022-12-01 15:29:02 +0800

    first draft complete

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,17 @@
 const down = document.querySelector("#down_icon");
-const aboutMe = document.querySelector("#about_me_title");
+const aboutMe = document.querySelector("#about_me");
 down.addEventListener("click", () => {
     aboutMe.scrollIntoView({ behavior: 'smooth'});
 })
+
+icons = document.querySelectorAll(".icon");
+icons.forEach(icon => {
+    icon.addEventListener("mouseenter", () => {
+        icon.classList.remove("fa-4x");
+        icon.classList.add("fa-5x")
+    })
+    icon.addEventListener("mouseleave", () => {
+        icon.classList.remove("fa-5x");
+        icon.classList.add("fa-4x")
+    })
+});
